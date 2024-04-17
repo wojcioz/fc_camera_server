7196fa731a6779f50016be2b0544c133029125da
Wojtek Zapał <wojciech.zapal@gmail.com>
2024-04-17 12:53:37 +0200

    Removed unused function, added utils

--- CameraHandler.py
+++ CameraHandler.py
@@ -34,26 +34,3 @@ class CameraHandler:
         print("Recording paused")
     def convert_vids(self):
         convert_videos("recs/20240416")
-def generate_frames():
-
-    global cam_handler
-    # time.sleep(0.1)  # allow the camera to warmup
-    encoder = H264Encoder(10000000)
-    while True:
-        time.sleep(0.1)
-        if not cam_handler.recording:
-            break
-        try:
-            # array = cam_handler.cam.capture_array()
-            
-            cam_handler.cam.start_recording(encoder, 'test.h264')
-            # _, jpeg = cv2.imencode('.jpg', array)
-            # yield (b'--frame\r\n'
-            #        b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
-            if not cam_handler.recording:
-                break
-        except Exception as e:
-            print(f"Error: {e}")
-            continue
-    cam_handler.cam.stop_recording()
-    print("Stopping frame generation")
