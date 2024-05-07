53b344a0822a79efee5eef5bb297a11bbf62c6b2
Wojtek Zapał <wojciech.zapal@gmail.com>
2024-05-07 12:30:45 +0200

    Latest RPi changes

--- CameraHandler.py
+++ CameraHandler.py
@@ -2,7 +2,7 @@ from picamera2 import MappedArray, Picamera2, Preview
 from libcamera import controls
 from picamera2.encoders import H264Encoder, Quality
 
-from fc_camera_server.utils import generate_filename, convert_videos
+from utils import generate_filename, convert_videos
 
 class CameraHandler:
     def __init__(self):
@@ -10,8 +10,6 @@ class CameraHandler:
         self.cam.configure(
             self.cam.create_video_configuration(main={"size": (1920, 1080),"format": "RGB888"},
                                                   lores={"size": (1920, 1080),"format": "YUV420"})
-            # self.cam.create_preview_configuration({"size": (1024, 768)})
-            # self.cam.create_preview_configuration({"size": (1440, 1080)})
         )
         self.mode = "lores"
         self.cam.set_controls({"AfMode": controls.AfModeEnum.Continuous, "FrameRate": 20})
@@ -19,13 +17,6 @@ class CameraHandler:
         self.encoder = H264Encoder(10000000)
         
 
-    def start(self):
-        self.cam.start()
-        self.recording = True
-
-    def stop(self):
-        # self.cam.stop()
-        self.recording = False
     def start_recording(self):
         self.cam.start_recording(self.encoder, generate_filename(".h264"),name=self.mode, quality=Quality.MEDIUM)
         print("Recording started")
